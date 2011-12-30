7befc623848545e3c5894515df65d72e3489119c
hadley <h.wickham@gmail.com>
2011-12-30 17:59:30 -0600

    More documentation

--- R/add-grob.r
+++ R/add-grob.r
@@ -2,10 +2,19 @@
 #' 
 #' Does not affect height/width
 #' 
+#' @param x a \code{\link{gtable}} object
 #' @param grobs a single grob or a list of grobs
+#' @param t a numeric vector giving the top extent of the grobs
+#' @param l a numeric vector giving the left extent of the grobs
+#' @param b a numeric vector giving the bottom extent of the grobs
+#' @param r a numeric vector giving the right extent of the grobs
+#' @param z a numeric vector giving the order in which the grobs should be
+#'   plotted
 #' @param clip should drawing be clipped to the specified cells
 #'   (\code{"on"}), the entire table (\code{"inherit"}), or not at all 
 #'   (\code{"off"})
+#' @param name name of the grob - used to modify the grob name before it's
+#'   plotted.
 #' @export
 gtable_add_grob <- function(x, grobs, t, l, b = t, r = l, z = 1, clip = "on", name = x$name) 
 {
