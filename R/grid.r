b1222df4e1ceee65d5d42d5c1f1240505b618aa4
Winston Chang <winston@stdout.org>
2012-07-19 16:07:36 -0500

    grid.draw.gtable: code cleanup

--- R/grid.r
+++ R/grid.r
@@ -30,38 +30,32 @@ heightDetails.gtable <- function(x) absolute.size(gtable_height(x))
 
 #' @S3method grid.draw gtable
 grid.draw.gtable <- function(x, recording = TRUE) {
-  # Return the viewport for a child grob in a gtable
-  child_vp <- function(grob, vp_name, t, r, b, l, clip) {
-    viewport(name = vp_name, layout.pos.row = t:b,
-             layout.pos.col = l:r, clip = clip)
-  }
-  
+
   children_vps <- mapply(child_vp,
-                         grob = x$grobs, vp_name = vpname(x$layout),
-                         t = x$layout$t, r = x$layout$r, b = x$layout$b, l = x$layout$l,
-                         clip = x$layout$clip,
-                         SIMPLIFY = FALSE)
-  
-  x$grobs <- mapply(function(grob, vp) {
-    grob$wrapvp <- vp
-    class(grob) <- c("gTableChild", class(grob))
-    grob$name <- vp$name
-    grob
-  },
-                    x$grobs,
-                    children_vps,
-                    SIMPLIFY = FALSE)
+    grob = x$grobs,
+    vp_name = vpname(x$layout),
+    t = x$layout$t, r = x$layout$r, b = x$layout$b, l = x$layout$l,
+    clip = x$layout$clip,
+    SIMPLIFY = FALSE)
+
+  x$grobs <- mapply(wrap_gtableChild, x$grobs, children_vps,
+    SIMPLIFY = FALSE)
+
   if (inherits(x, "gTableChild")) {
-    gt <- gTree(children = do.call("gList", x$grobs), cl = c("gTableParent", "gTableChild"),
-                vp = x$vp, wrapvp = x$wrapvp, layoutvp = viewport(layout = gtable_layout(x), name = x$name))
+    gt <- gTree(children = do.call("gList", x$grobs),
+      cl = c("gTableParent", "gTableChild"),
+      vp = x$vp,
+      wrapvp = x$wrapvp,
+      layoutvp = viewport(layout = gtable_layout(x), name = x$name))
+  } else {
+    gt <- gTree(children = do.call("gList", x$grobs),
+      cl = c("gTableParent"),
+      vp = x$vp,
+      layoutvp = viewport(layout = gtable_layout(x), name = x$name))
   }
-  else {
-    gt <- gTree(children = do.call("gList", x$grobs), cl = c("gTableParent"),
-                vp = x$vp, layoutvp = viewport(layout = gtable_layout(x), name = x$name))
-   
-  }
+
   grid.draw(gt)
-  invisible(gt)
+  invisible()
 }
 
 #' @S3method grid.draw gTableChild
@@ -79,4 +73,20 @@ preDrawDetails.gTableParent <- function(x) {
 #' @S3method postDrawDetails gTableParent
 postDrawDetails.gTableParent <- function(x) {
   upViewport(recording = FALSE)
+}
+
+
+# Return the viewport for a child grob in a gtable
+child_vp <- function(grob, vp_name, t, r, b, l, clip) {
+  viewport(name = vp_name, layout.pos.row = t:b,
+           layout.pos.col = l:r, clip = clip)
+}
+
+# Turn a grob into a gtableChild, and store information about the
+# viewport used within the gtable
+wrap_gtableChild <- function(grob, vp) {
+  grob$wrapvp <- vp
+  grob$name <- vp$name
+  class(grob) <- c("gTableChild", class(grob))
+  grob
 }
