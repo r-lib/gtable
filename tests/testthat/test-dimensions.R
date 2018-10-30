deaba1715d20115b73a2e3f12a2d11e911e17c81
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-10-30 10:37:21 +0100

    Remove nrow_/ncol_ tests

--- tests/testthat/test-dimensions.R
+++
@@ -1,8 +0,0 @@
-context("Dimensions")
-
-test_that("nrow_ and ncol_ gives equal result to nrow and ncol", {
-  base <- gtable(unit(rep(1, 5), "null"), unit(rep(1, 3), "null"))
-
-  expect_equal(nrow(base), nrow_(base))
-  expect_equal(ncol(base), ncol_(base))
-})
